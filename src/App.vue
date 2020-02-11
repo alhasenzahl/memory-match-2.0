34fd2f1e4096d09a7717bb077484571781a8368f
Amanda Hasenzahl <amandahasenzahl@Amandas-MacBook-Pro.local>
2020-02-11 12:32:14 -0600

    Limit the number of cards that can be clicked to 2

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@
                 <span id="stop-watch">00:00</span>
             </section>
             <ul class="deck">
-                <li class="card" :class="{'open': card.isOpen, 'show': card.isShown}" :key="card.key" v-for="card in cards" @click="flipCards(card)">
+                <li class="card" :class="{'open': card.isOpen, 'show': card.isShown, 'match': card.isMatched}" :key="card.key" v-for="card in cards" @click="flipCards(card)">
                     <i :class="card.symbol"></i>
                 </li>
             </ul>
@@ -45,23 +45,25 @@ export default {
     data: () => {
         return {
             cards: [
-                { symbol: 'fa fa-anchor', key: 1, isOpen: false, isShown: false },
-                { symbol: 'fa fa-anchor', key: 2, isOpen: false, isShown: false },
-                { symbol: 'fa fa-bomb', key: 3, isOpen: false, isShown: false },
-                { symbol: 'fa fa-bomb', key: 4, isOpen: false, isShown: false },
-                { symbol: 'fa fa-bicycle', key: 5, isOpen: false, isShown: false },
-                { symbol: 'fa fa-bicycle', key: 6, isOpen: false, isShown: false },
-                { symbol: 'fa fa-diamond', key: 7, isOpen: false, isShown: false },
-                { symbol: 'fa fa-diamond', key: 8, isOpen: false, isShown: false },
-                { symbol: 'fa fa-paper-plane-o', key: 9, isOpen: false, isShown: false },
-                { symbol: 'fa fa-paper-plane-o', key: 10, isOpen: false, isShown: false },
-                { symbol: 'fa fa-cube', key: 11, isOpen: false, isShown: false },
-                { symbol: 'fa fa-cube', key: 12, isOpen: false, isShown: false},
-                { symbol: 'fa fa-leaf', key: 13, isOpen: false, isShown: false },
-                { symbol: 'fa fa-leaf', key: 14, isOpen: false, isShown: false },
-                { symbol: 'fa fa-bolt', key: 15, isOpen: false, isShown: false },
-                { symbol: 'fa fa-bolt', key: 16, isOpen: false, isShown: false }
+                { symbol: 'fa fa-anchor', key: 1, isOpen: false, isShown: false, isMatched: false },
+                { symbol: 'fa fa-anchor', key: 2, isOpen: false, isShown: false, isMatched: false },
+                { symbol: 'fa fa-bomb', key: 3, isOpen: false, isShown: false, isMatched: false },
+                { symbol: 'fa fa-bomb', key: 4, isOpen: false, isShown: false, isMatched: false },
+                { symbol: 'fa fa-bicycle', key: 5, isOpen: false, isShown: false, isMatched: false },
+                { symbol: 'fa fa-bicycle', key: 6, isOpen: false, isShown: false, isMatched: false },
+                { symbol: 'fa fa-diamond', key: 7, isOpen: false, isShown: false, isMatched: false },
+                { symbol: 'fa fa-diamond', key: 8, isOpen: false, isShown: false, isMatched: false },
+                { symbol: 'fa fa-paper-plane-o', key: 9, isOpen: false, isShown: false, isMatched: false },
+                { symbol: 'fa fa-paper-plane-o', key: 10, isOpen: false, isShown: false, isMatched: false },
+                { symbol: 'fa fa-cube', key: 11, isOpen: false, isShown: false, isMatched: false },
+                { symbol: 'fa fa-cube', key: 12, isOpen: false, isShown: false, isMatched: false },
+                { symbol: 'fa fa-leaf', key: 13, isOpen: false, isShown: false, isMatched: false },
+                { symbol: 'fa fa-leaf', key: 14, isOpen: false, isShown: false, isMatched: false },
+                { symbol: 'fa fa-bolt', key: 15, isOpen: false, isShown: false, isMatched: false },
+                { symbol: 'fa fa-bolt', key: 16, isOpen: false, isShown: false, isMatched: false }
             ],
+            openCards: [],
+            clickedCards: []
         }
     },
     created() {
@@ -83,19 +85,23 @@ export default {
         },
         flipCards(card) {
             // IF card isn't OPEN, SHOWN, or MATCHED 
-            if (! card.isOpen && !card.isShown) {
-                card.isOpen = ! card.isOpen;
-                card.isShown = ! card.isShown;
-            }
+            if (! card.isOpen && ! card.isShown && ! card.isMatched) {
                 // Start timer
                 // Push card into openCards array
+                this.openCards.push(card);
 
                 // IF openCards array length is 2 or less
+                if (this.openCards.length <= 2) {
                     // Add OPEN and SHOWN to card
+                    card.isOpen = ! card.isOpen;
+                    card.isShown = ! card.isShown;
                     // push card to clickedCards array
+                    this.clickedCards.push(card);
                     // call matchCards() function
                     // call clickCounter() function
                     // call starCount() function
+                }
+            }
         }
     }
 }
