b9b8f6a7a91b418a2dc76c743e1cfa63e8fe2c98
Amanda Hasenzahl <amandahasenzahl@Amandas-MacBook-Pro.local>
2020-02-11 07:28:26 -0600

    Add in static template from original project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="container">
+      <header>
+          <h1>Matching Game</h1>
+      </header>
+
+      <section class="score-panel">
+        <ul class="stars">
+          <li class="star-icon"><i class="fa fa-star"></i></li>
+          <li class="star-icon"><i class="fa fa-star"></i></li>
+          <li class="star-icon"><i class="fa fa-star"></i></li>
+        </ul>
+        <span class="moves">0</span> Moves
+
+          <div class="restart">
+          <i class="fa fa-repeat"></i>
+        </div>
+      </section>
+      <section>
+          <span id="stop-watch">00:00</span>
+      </section>
+      <ul class="deck"></ul>
+    </div>
+    <div class="modal-background hide">
+      <div class="modal-body">
+          <div class="modal-body_container">
+              <div class="modal-heading">
+                  <h2 class="modal-title">Game Stats</h2>
+              </div>
+              <div class="modal-main">
+                  <h3 class="modal-main_title"></h3>
+                  <p class="modal-main_paragraph"></p>
+              </div>
+              <div class="modal-buttons">
+                  <button class="quit-game">Quit</button>
+                  <button class="play-again">Play Again</button>
+              </div>
+          </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
